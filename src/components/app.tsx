5d41207346b689be9f341b29387dfa29685cdf83
alevchenko <o.levchenko@hotmail.com>
2017-01-24 16:27:43 +0200

    small fixes

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,11 +1,7 @@
 import * as React from 'react';
 
-import { HelloWorld } from './hello-world/hello.component';
-
 export class App extends React.Component<{},{}> {
   render() {
-    return (
-      <HelloWorld message={ Math.random() }/>
-    );
+    return <h1>Hello world</h1>
   }
 }
--- src/interfaces/hello-world.tsx
+++
@@ -1,3 +0,0 @@
-export interface HelloProps {
-  message: number;
-}
